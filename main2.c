c609fc3f215b39a57a8e290021322ee13df84799
FelipePetronacci <petronaccif@gmail.com>
2025-08-29 10:23:49 -0300

    Update main2.c

--- main2.c
+++ main2.c
@@ -1,23 +1,32 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int main(int argc, char *argv[]){
+int main(int argc, char* argv[]){
     FILE *fp;
-    char *filename= argv[1]; 
+    char *filename = argv[1]; 
     fp = fopen(filename ,"r");
-    char buffer[1025];
-    while(fgets(buffer, 1025, fp) != NULL){
-        for(int i; buffer[i] != EOF; i++){
-            if(buffer[i] > 96 && buffer[i] < 123){
-
-            } else if(buffer[i] > 48 && buffer[i] < 58) {
-                for (int i = 0; i < (buffer[i] - '0'); i++){
-                    putchar(buffer[i-1]);
+    char c, letra = 0;
+    int count = 0;
+    while((c = fgetc(fp)) != EOF){
+        if(c >= '0' && c <= '9'){
+             count = count * 10 + (c - '0');
+        } else {
+            if(letra != 0){ 
+                if(count == 0) count = 1;
+                for(int i = 0; i < count; i++){
+                    putchar(letra);
                 }
-                
+                count = 0;
             }
+            letra = c;
         }
-        
-        
     }
+    if(letra != 0){
+        if(count == 0) count = 1;
+        for(int i = 0; i < count; i++){
+            putchar(letra);
+        }
+    }
+    fclose(fp);
+    return 0;
 }
